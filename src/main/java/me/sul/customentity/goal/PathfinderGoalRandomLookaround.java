3a359ef73144b649c17863d15b6683487fbe5a00
SuL <jjw011202@naver.com>
2020-09-07 18:09:51 +0900

    PathfinderGoalRandomLookaround canUse()의 기준 추가

--- src/main/java/me/sul/customentity/goal/PathfinderGoalRandomLookaround.java
+++ src/main/java/me/sul/customentity/goal/PathfinderGoalRandomLookaround.java
@@ -5,15 +5,18 @@ import net.minecraft.server.v1_12_R1.EntityInsentient;
 import java.util.Random;
 
 public class PathfinderGoalRandomLookaround extends net.minecraft.server.v1_12_R1.PathfinderGoalRandomLookaround {
-    private Random random = new Random();
+    private final EntityInsentient nmsEntity;
     private final float chance;
+
+    private Random random = new Random();
     public PathfinderGoalRandomLookaround(EntityInsentient entityInsentient, float chance) {
         super(entityInsentient);
+        this.nmsEntity = entityInsentient;
         this.chance = chance;
     }
 
     @Override
     public boolean a() { // canUse()
-        return random.nextFloat() < chance;
+        return random.nextFloat() < chance && nmsEntity.getGoalTarget() == null;
     }
 }
