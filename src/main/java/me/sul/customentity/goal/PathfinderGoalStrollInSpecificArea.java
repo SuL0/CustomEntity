1eae1f3d0395178c7cb4e18382f831153534c4cf
SuL <jjw011202@naver.com>
2020-09-11 17:56:10 +0900

    StrollInSpecificArea이 PathfinderGoal -> EasilyModifiedPathfinderGoal을 상속하도록 변경

--- src/main/java/me/sul/customentity/goal/PathfinderGoalStrollInSpecificArea.java
+++ src/main/java/me/sul/customentity/goal/PathfinderGoalStrollInSpecificArea.java
@@ -2,13 +2,13 @@ package me.sul.customentity.goal;
 
 import me.sul.customentity.spawnarea.Area;
 import me.sul.customentity.spawnarea.AreaUtil;
-import net.minecraft.server.v1_12_R1.*;
-import org.bukkit.Location;
+import net.minecraft.server.v1_12_R1.EntityCreature;
+import net.minecraft.server.v1_12_R1.GenericAttributes;
+import net.minecraft.server.v1_12_R1.Navigation;
 import org.bukkit.entity.Entity;
 
 // PathfinderGoalMoveThroughVillage 기반
-// TODO: EasilyModifiedPathfinderGoal를 상속하도록 코드 변경
-public class PathfinderGoalStrollInSpecificArea<T extends EntityCreature> extends PathfinderGoal {
+public class PathfinderGoalStrollInSpecificArea<T extends EntityCreature> extends EasilyModifiedPathfinderGoal {
     private final T nmsEntity;
     private final Entity bukkitEntity;
     private final Area area;
@@ -16,31 +16,31 @@ public class PathfinderGoalStrollInSpecificArea<T extends EntityCreature> extend
     private final int randomInterval;
 
     public PathfinderGoalStrollInSpecificArea(T nmsEntity, Area area, double speed, int randomInterval) {
+        super(nmsEntity);
         this.nmsEntity = nmsEntity;
         this.bukkitEntity = nmsEntity.getBukkitEntity();
         this.area = area;
         this.speed = speed;
         this.randomInterval = randomInterval;
-        this.a(1); // setFlag() ? 모든게 다 a(1)임
-        if (!(nmsEntity.getNavigation() instanceof Navigation)) {
+        setFlag(Flag.LOOK);
+        if (!(getNavigation() instanceof Navigation)) {
             throw new IllegalArgumentException("Unsupported mob for MoveThroughVillageGoal");
         }
     }
 
-
     @Override
-    public boolean a() {  // canUse()
-        if (!nmsEntity.getNavigation().o() || nmsEntity.getGoalTarget() != null) return false;
-        return nmsEntity.getRandom().nextInt(randomInterval) == 0;
+    public boolean canUse() {
+        if (!isNavigationDone() || getGoalTarget() != null) return false;
+        return getRandom().nextInt(randomInterval) == 0;
     }
 
     @Override
-    public boolean b() {  // canContinueToUse()
-        return !nmsEntity.getNavigation().o() && nmsEntity.getGoalTarget() == null;
+    public boolean canContinueToUse() {
+        return !isNavigationDone() && getGoalTarget() == null;
     }
 
     @Override
-    public void c() {  // start()
+    public void start() {
         org.bukkit.Location destinationLoc;
         // 지정 구역에서 벗어났을 때
         if (AreaUtil.isAwayFromArea(area, bukkitEntity.getLocation())) {
@@ -49,22 +49,15 @@ public class PathfinderGoalStrollInSpecificArea<T extends EntityCreature> extend
                 nmsEntity.killEntity();
                 return;
             }
-            moveToLocConsideringDoor(destinationLoc, speed);
+            moveToLoc(destinationLoc, speed, true);
         } else {
             destinationLoc = AreaUtil.getLocationForStroll(area, bukkitEntity.getLocation(), 5,10);
-            moveToLocConsideringDoor(destinationLoc, speed*1.2);
+            moveToLoc(destinationLoc, speed*1.2, true);
         }
     }
 
-    private void moveToLocConsideringDoor(Location destinationLoc, double speed) {
-        Navigation navigation = (Navigation) nmsEntity.getNavigation();
-        navigation.a(true);
-        PathEntity goalPath = navigation.a(destinationLoc.getX(), destinationLoc.getY(), destinationLoc.getZ());
-        nmsEntity.getNavigation().a(goalPath, speed);
-    }
-
     @Override
-    public void d() {  // stop()
-        nmsEntity.getNavigation().p(); // stop
+    public void stop() {
+        stopNavigation();
     }
 }
